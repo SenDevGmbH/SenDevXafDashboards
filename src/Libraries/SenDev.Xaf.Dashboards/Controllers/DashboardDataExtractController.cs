8aeb7fc708fcb605f537ac7492870bbaa1f54e9d
mikhail.tuminskiy <mikhail.tuminskiy@sendev.gmbh>
2021-11-12 17:23:12 +0300

    WIP | Added dashboard data extract controller

---
+++ src/Libraries/SenDev.Xaf.Dashboards/Controllers/DashboardDataExtractController.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using DevExpress.ExpressApp;
+using SenDev.Xaf.Dashboards.BusinessObjects;
+using SenDev.Xaf.Dashboards.Scripting;
+
+namespace SenDev.Xaf.Dashboards.Controllers
+{
+	public class DashboardDataExtractController : ObjectViewController<ObjectView, IDashboardDataExtract>
+	{
+		private ScriptCompilationHelper compiler = null;
+
+
+		protected override void OnAfterConstruction()
+		{
+			base.OnAfterConstruction();
+			//compiler = new ScriptCompilationHelper(AssembliesHelper.GetReferencedAssembliesPaths(Application));
+		}
+
+
+		protected override void OnActivated()
+		{
+			base.OnActivated();
+			ObjectSpace.ObjectSaving += ObjectSaving;
+		}
+
+		protected override void OnDeactivated()
+		{
+			base.OnDeactivated();
+			ObjectSpace.ObjectSaving -= ObjectSaving;
+		}
+
+
+		private void ObjectSaving(object sender, ObjectManipulatingEventArgs e)
+		{
+			var script = (e.Object as IDashboardDataExtract).Script;
+			var tempFile = Path.GetTempFileName();
+			var isCompiled = compiler.Compile(script, tempFile).Success;
+			File.Delete(tempFile);
+
+			if (!isCompiled)
+			{
+				ObjectSpace.RemoveFromModifiedObjects(e.Object);
+			}
+		}
+	}
+}
---
+++ src/Libraries/UnitTests/DashboardDataExtractControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using SenDev.Xaf.Dashboards.Controllers;
+using Xunit;
+
+namespace UnitTests
+{
+	public class DashboardDataExtractControllerTests : XpoTestBase
+	{
+		[Fact]
+		public void Test1()
+		{
+			using (var application = XpoInMemoryXafApplication.CreateInstance())
+			{
+				var controller = application.CreateController<DashboardDataExtractController>();
+				
+				Assert.NotNull(controller);
+			}
+		}
+	}
+}
