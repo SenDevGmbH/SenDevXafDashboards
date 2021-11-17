2b8353be87e6bb877c3646a07e0b041dbf41dba2
mikhail.tuminskiy <mikhail.tuminskiy@sendev.gmbh>
2021-11-17 13:14:56 +0300

    Added unit tests for the DashboardDataExtractController class

--- src/Libraries/UnitTests/DashboardDataExtractControllerTests.cs
+++ src/Libraries/UnitTests/DashboardDataExtractControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using SenDev.Xaf.Dashboards.Controllers;
+﻿using DevExpress.ExpressApp;
+using SenDev.Xaf.Dashboards.BusinessObjects;
+using SenDev.Xaf.Dashboards.Controllers;
 using Xunit;
 
 namespace UnitTests
@@ -6,13 +8,70 @@ namespace UnitTests
 	public class DashboardDataExtractControllerTests : XpoTestBase
 	{
 		[Fact]
-		public void Test1()
+		public void CompilerInitializeTest()
 		{
 			using (var application = XpoInMemoryXafApplication.CreateInstance())
 			{
-				var controller = application.CreateController<DashboardDataExtractController>();
+				var collectionSource = new CollectionSource(application.CreateObjectSpace(), typeof(DashboardDataExtract));
+				var listView = new ListView(collectionSource, application, true);
 				
-				Assert.NotNull(controller);
+				var controller = application.CreateController<DashboardDataExtractController>();
+				controller.SetView(listView);
+
+				controller.Active.Clear();
+				controller.Active["Test"] = true;
+
+				Assert.NotNull(controller.Compiler);
+			}
+		}
+
+		[Fact]
+		public void SaveInvalidScriptTest()
+		{
+			using (var application = XpoInMemoryXafApplication.CreateInstance())
+			using (var objectSpace = application.CreateObjectSpace())
+			{
+				var collectionSource = new CollectionSource(objectSpace, typeof(DashboardDataExtract));
+				var listView = new ListView(collectionSource, application, true);
+
+				var controller = application.CreateController<DashboardDataExtractController>();
+				controller.SetView(listView);
+
+				controller.Active.Clear();
+				controller.Active["Test"] = true;
+
+				var dataExtract = objectSpace.CreateObject<DashboardDataExtract>();
+				dataExtract.Script = "invalid script";
+				objectSpace.CommitChanges();
+
+				Assert.Empty(objectSpace.GetObjects<DashboardDataExtract>());
+			}
+		}
+
+		[Fact]
+		public void SaveValidScriptTest()
+		{
+			using (var application = XpoInMemoryXafApplication.CreateInstance())
+			using (var objectSpace = application.CreateObjectSpace())
+			{
+				var collectionSource = new CollectionSource(objectSpace, typeof(DashboardDataExtract));
+				var listView = new ListView(collectionSource, application, true);
+
+				var controller = application.CreateController<DashboardDataExtractController>();
+				controller.SetView(listView);
+
+				controller.Active.Clear();
+				controller.Active["Test"] = true;
+
+				var dataExtract = objectSpace.CreateObject<DashboardDataExtract>();
+				dataExtract.Script = @"
+					public class MyClass
+					{
+						public int MyMethod() { return 42; }
+					}";
+				objectSpace.CommitChanges();
+
+				Assert.Single(objectSpace.GetObjects<DashboardDataExtract>());
 			}
 		}
 	}
