598f30878e526b11af08e7ffde61602ee39c015a
Sergej Derjabkin <sergej@sendev.de>
2022-04-04 20:39:16 +0200

    - Fixed build issues

--- src/Libraries/SenDev.Xaf.Dashboards.Win/Properties/AssemblyInfo.cs
+++ src/Libraries/SenDev.Xaf.Dashboards.Win/Properties/AssemblyInfo.cs
@@ -4,11 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SenDev.Xaf.Dashboards.Win")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SenDev.Xaf.Dashboards.Win")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
