0abb74142309da1bf2319fab745702bab3b5bec4
Sergej Derjabkin <sergej@sendev.de>
2022-08-25 15:07:42 +0200

    - Removed ForceClean - Changed DX Version for build

--- src/Demo/ForceClean.bat
+++
@@ -1,4 +0,0 @@
-REM "Removes all obj and bin folders in the project, doing Visual Studio Build-Clean job, but better."
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-pause
