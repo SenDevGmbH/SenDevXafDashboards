ba19ff932549f53dc643d353f76c8a583b701c45
Sergej Derjabkin <sergej@sendev.de>
2024-03-10 09:42:18 +0100

    - Use conditional compilation to use cs-script only in .net core projects

--- src/Libraries/SenDev.Xaf.Dashboards/Scripting/ScriptCompilationHelper.cs
+++ src/Libraries/SenDev.Xaf.Dashboards/Scripting/ScriptCompilationHelper.cs
@@ -20,17 +20,73 @@ namespace SenDev.Xaf.Dashboards.Scripting
 	{
 
 
-		public ScriptCompilationHelper()
+		private static readonly ConcurrentDictionary<string, Assembly> scriptAssemblyCache = new ConcurrentDictionary<string, Assembly>();
+
+		private static readonly IList<string> namespaces = new List<string>() { "System" };
+
+		private static readonly CSharpCompilationOptions compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+			.WithUsings(namespaces);
+
+		private readonly IList<MetadataReference> references = new List<MetadataReference>()
+		{
+			MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+			MetadataReference.CreateFromFile(typeof(GCSettings).Assembly.Location),
+			MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+			MetadataReference.CreateFromFile(typeof(CriteriaOperator).Assembly.Location),
+			MetadataReference.CreateFromFile(typeof(IDataReader).Assembly.Location),
+			MetadataReference.CreateFromFile(typeof(BitArray).Assembly.Location),
+			MetadataReference.CreateFromFile(typeof(Queryable).Assembly.Location)
+		};
+
+		public ScriptCompilationHelper(string[] referencedAssemblies)
+		{
+			foreach (var assembly in referencedAssemblies)
+			{
+				references.Add(MetadataReference.CreateFromFile(assembly));
+			}
+		}
+
+		public ScriptCompilationHelper() : this(Array.Empty<string>())
 		{
 
 		}
-		public ScriptCompilationHelper(string[] referenceAssemblies)
-		{
 
+		public EmitResult Compile(string script, string outputPath)
+		{
+			var syntaxTree = CSharpSyntaxTree.ParseText(script);
+
+			var compilation = CSharpCompilation.Create(Path.GetFileName(outputPath), new SyntaxTree[] { syntaxTree }, references, compilationOptions);
+
+			return compilation.Emit(outputPath);
 		}
 
 
+		private Assembly GetScriptAssembly(string script)
+		{
+			if (scriptAssemblyCache.TryGetValue(script, out var cachedAssembly))
+				return cachedAssembly;
 
+			var assemblyFilePath = Path.GetTempFileName();
+
+			var compileResult = Compile(script, assemblyFilePath);
+
+			if (compileResult.Success)
+			{
+				var assembly = Assembly.Load(File.ReadAllBytes(assemblyFilePath));
+				File.Delete(assemblyFilePath);
+				scriptAssemblyCache[script] = assembly;
+				return assembly;
+			}
+
+			var errors = compileResult.Diagnostics
+				.Where(diagnostic => diagnostic.Severity == DiagnosticSeverity.Error)
+				.Select(diagnostic => diagnostic.ToString());
+
+			throw new InvalidOperationException("Compilation failed:\n" + string.Join("\n", errors));
+		}
+
+
+#if NET5_0_OR_GREATER
 		public dynamic CreateObject(string script)
 		{
 
@@ -44,8 +100,13 @@ namespace SenDev.Xaf.Dashboards.Scripting
 				throw new InvalidOperationException("Compilation failed:\n" + ex.Message, ex);
 			}
 		}
-
-
+#else
+		public dynamic CreateObject(string script)
+		{
+			var assembly = GetScriptAssembly(script);
+			return assembly.CreateInstance(assembly.GetExportedTypes().Single().FullName);
+		}
+#endif
 
 
 
