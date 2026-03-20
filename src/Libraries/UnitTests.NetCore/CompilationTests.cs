d9032dce3caf1ce08d7e421c1bcd52872d66a24f
Sergej Derjabkin <sergej@sendev.de>
2026-03-20 10:48:19 +0100

    - Fixed paths to SenDev.Xaf.Dashboards.CSScriptCompiler

--- src/Libraries/UnitTests.NetCore/CompilationTests.cs
+++ src/Libraries/UnitTests.NetCore/CompilationTests.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using SenDev.Xaf.Dashboard.CSScriptCompiler;
 using SenDev.Xaf.Dashboards;
 using SenDev.Xaf.Dashboards.BusinessObjects;
+using SenDev.Xaf.Dashboards.CSScriptCompiler;
 using SenDev.Xaf.Dashboards.Scripting;
 using Xunit;
 
